e3321da25fbb239cdfd8b1365adc2daa03db2e6a
AshokVunnam <97439244+AshokVunnam@users.noreply.github.com>
2023-05-29 12:57:39 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-	  System.out.println("How are you?2")
+	  System.out.println("How are you?2");
     }
 }
